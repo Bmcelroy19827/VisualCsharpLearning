a7ea924f5f519dae9ce9f7eee7cc9ae2de0cb1b4
bmcel19827 <angelasman2004@yahoo.com>
2019-09-05 06:30:16 -0400

    Addedmodule1 and module2

---
+++ Modules/Module01SyntaxReview/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01SyntaxReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Iteration Logic
+
+            // for loop
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(string.Format("Iteration number {0}", i));
+            }
+
+            // foreach loop
+            string[] names = { "Jamie", "Bryan", "David", "Mark", "Tessa", "Trisha" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // while loop
+            Console.WriteLine("Type 'enter' if you want to enter data to nowhere");
+            string userResponse = Console.ReadLine();
+            if (userResponse == "enter")
+            {
+                while(userResponse != "stop")
+                {
+                    Console.WriteLine("Enter Echo data now: (type 'stop' to end)");
+                    userResponse = Console.ReadLine();
+                    Console.WriteLine(string.Format("{0} echoed.",userResponse));
+
+                }
+
+                Console.WriteLine("While loop exited\n\n\n");
+            }
+
+            // Do loop - notice that userResponse will be 'stop' when loop starts but will still execute once.
+            do
+            {
+                Console.WriteLine(string.Format("Beginning of Do Loop. User Response = {0}", userResponse));
+                Console.WriteLine("Do Loop data to enter: ('stop' to stop the do)");
+                userResponse = Console.ReadLine();
+                Console.WriteLine(string.Format("UserResponse = {0} at end of the do loop",userResponse));
+            } while (userResponse != "stop");
+
+            #endregion
+
+            #region Arrays
+
+            int[] numberArray = new int[10];
+            int[] initializedAndAssigned = { 1, 2, 3, 4, 5, 6 };
+
+            int[,] twoDimensional = new int[10, 10];
+
+            // Note the size of the arrays contained inside initial array do not need to be specified.
+            int[][] jaggedArray = new int[10][];
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[7];
+
+            #endregion
+        }
+    }
+}
---
+++ Modules/Module02MethodsExceptionsAndMonitoring/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Module02MethodsExceptionsAndMonitoring")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module02MethodsExceptionsAndMonitoring")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7bd41a0-3d34-435e-9fa4-3b8258b2bd4c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
